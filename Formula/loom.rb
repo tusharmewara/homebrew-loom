b5742e089b3d32fe7f67de8e2c90bae215d07696
Tushar <tmewara@Tushars-MacBook-Pro.local>
2026-05-29 20:43:27 +0530

    loom v0.1.3: fix bottle cellar prefix structure

--- Formula/loom.rb
+++ Formula/loom.rb
@@ -9,8 +9,8 @@ class Loom < Formula
 
   bottle do
     root_url "https://github.com/tusharmewara/homebrew-loom/releases/download/v0.1.3"
-    sha256 cellar: :any, sonoma: "caed701efca00d161cd4eab85d0228030272fe334d1fd405ad7167b53316c816"
-    sha256 cellar: :any, ventura: "c09859d0647600e466199705c2682b8466e5ad062694536b1caa9c725fee8642"
+    sha256 cellar: :any, sonoma: "b8bff9cc6630df4441cf59c16f93600e4ed33be2d46a2d3a3c1317a1ef1827da"
+    sha256 cellar: :any, ventura: "a876d153bdb7cfff3c92d402f2d82ead9dda5d870a2ce2857f7cb01ec89590bc"
   end
 
   def install
